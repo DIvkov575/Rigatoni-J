56722e2f3e05a893bfad270fe22877ecfd9613e8
Divkov575 <dmitriy@ivkov.net>
2024-11-17 22:04:05 -0500

    added intialization scheduling - TODO: add next-event scheduling

--- src/main/java/org/divkov/rigatoni/Main.java
+++ src/main/java/org/divkov/rigatoni/Main.java
@@ -4,8 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 class Main {
     static void random_sleep(int a, int b) {
@@ -21,23 +26,65 @@ class Main {
         ObjectMapper objectMapper = new ObjectMapper();
         Driver[] drivers= objectMapper.readValue(new File("./assets/accounts.json"), Driver[].class);
 
-
-
-
-
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
         Arrays.stream(Arrays.copyOfRange(drivers, 0, 1)).parallel()
-                .forEach(driver -> {
-                    driver.createDriver();
+            .forEach(driver -> {
 
-                    driver.authenticate();
-                    Main.random_sleep(4, 6);
+                // schedule initialization
+                scheduler.schedule(() -> {
+                    for (Driver driver2: drivers) {
+                        if (driver2.getUsername().equals(driver.getUsername())) {
+                            System.out.println("Driver" + driver2.getUsername() + "initialized at" + LocalTime.now());
+                            driver2.createDriver();
+                            driver2.authenticate();
+                            Main.random_sleep(4, 6);
 
-                    driver.navigate(driver.getPlayables()[0]);
-                    Main.random_sleep(4, 6);
+                            driver.navigate(driver.getPlayables()[0]);
+                            Main.random_sleep(4, 6);
 
-                    driver.play();
-                });
+                            driver.play();
+                            // TODO: schedule next event
+                        }
+                    }
+                }, (int) (Math.random() * 120 * 60_000), TimeUnit.MILLISECONDS);
+            });
+
+
+        scheduler.shutdown();
+
+
+//        double variance = ((Math.random() - 0.5)*2) * 30;
+//
+//        LocalTime targetTime = LocalTime.now().plusSeconds(10);
+//        long delay = Duration.between(LocalTime.now(), targetTime).toMillis();
+//        scheduler.schedule(() -> {
+//            System.out.println("Task 1executed at: " + LocalTime.now());
+//        }, delay, TimeUnit.MINUTES);
+//
+//        LocalTime targetTime2 = LocalTime.now().plusSeconds(5);
+//        long delay2 = Duration.between(LocalTime.now(), targetTime2).toMillis();
+//        scheduler.schedule(() -> {
+//            System.out.println("Task 2executed at: " + LocalTime.now());
+//        }, delay2, TimeUnit.MILLISECONDS);
+//
+//        // Shut down the scheduler after the task runs
+
+
+
+
+//        Arrays.stream(Arrays.copyOfRange(drivers, 0, 1)).parallel()
+//                .forEach(driver -> {
+//                    driver.createDriver();
+//
+//                    driver.authenticate();
+//                    Main.random_sleep(4, 6);
+//
+//                    driver.navigate(driver.getPlayables()[0]);
+//                    Main.random_sleep(4, 6);
+//
+//                    driver.play();
+//                });
     }
 
 
